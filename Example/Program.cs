9b8145827f0a97013b0a04f8c74afa9e2c9aa071
Jacob Harvey <jharvey@mahalobanking.com>
2023-09-03 21:14:41 -0600

    learn about char array

--- Example/Program.cs
+++ Example/Program.cs
@@ -5,13 +5,15 @@ namespace ExampleProj
    
     class Program
     {
+
         public static void Main(string[] args)
         {
-            string s1 = "HEY";
-            string s2 = "There";
-            string s3 = s1 + " " + s2;
-            s3 = "!";
-            System.Diagnostics.Debug.WriteLine(s3);
+            char char1 = 'c';
+            char char2 = '\n';
+
+            //s1 += "d"; is really
+            System.Char[] s2 = new System.Char[] { 'a', 'b', 'c', 'd' };
+            System.Diagnostics.Debug.WriteLine(s2);
         }
     }
 }
