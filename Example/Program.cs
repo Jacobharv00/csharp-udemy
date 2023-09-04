4164f214a9a67f3880180f6a347e9dcba7b21f4b
Jacob Harvey <jharvey@mahalobanking.com>
2023-09-03 20:48:49 -0600

    new commit testing

--- Example/Program.cs
+++ Example/Program.cs
@@ -2,53 +2,13 @@
 
 namespace ExampleProj
 {
-    class Box
-    {
-        private int length;
-        private int width;
-        private int height;
-
-        public Box(int length, int width, int height)
-        {
-            this.length = length;
-            this.width = width;
-            this.height = height;
-        }
-
-        public int GetLength()
-        {
-            return length;
-        }
-
-        public int GetWidth()
-        {
-            return width;
-        }
-
-        public int GetHeight()
-        {
-            return height;
-        }
-
-        public static Box operator +(Box box1, Box box2)
-        {
-            return new Box(box1.GetLength() + box2.GetLength(),
-                box1.GetWidth() + box2.GetWidth(),
-                box1.GetHeight() + box2.GetHeight());
-        }
-    }
-
+   
     class Program
     {
         public static void Main(string[] args)
         {
-            Box box1 = new(6, 6, 6);
-            Box box2 = new(4, 4, 4);
-            Box box3 = box1 + box2;
-
-            System.Diagnostics.Debug.WriteLine("Length: " + box3.GetLength());
-            System.Diagnostics.Debug.WriteLine("Width: " + box3.GetWidth());
-            System.Diagnostics.Debug.WriteLine("Height: " + box3.GetHeight());
+            Type sType = typeof(string);
+            System.Diagnostics.Debug.WriteLine(sType.FullName);
         }
     }
 }
