6d171453d7ac7ffe9fd53e9c26d83c99a60f999d
Jacob Harvey <jharvey@mahalobanking.com>
2023-09-17 12:50:07 -0600

    feat: add linearSearch & binarySearchalgos

---
+++ Example/LinearSearch.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Example
+
+{
+	class LinearSearch
+	{
+		/// <summary>
+		/// Does a linear search of a list of elements
+		/// </summary>
+		static int linearSearch(List<int> elements, int x)
+		{
+			int length = elements.Count();
+
+			for (int i = 0; i < length; i++)
+			{
+				if (elements[i] == x)
+				{
+					return i;
+				}
+			}
+
+			return -1;
+		}
+	}
+
+}
+
